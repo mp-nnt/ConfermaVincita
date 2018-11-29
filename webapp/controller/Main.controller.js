63d7eddf8203b43eeaccb19663c7fba42a4547f0
S0020034726 <michele.pigozzo@nttdata.com>
2018-11-29 17:56:32 +0000

    Demo App

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,17 +1,18 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
+], function (Controller, JSONModel) {
 	"use strict";
 
 	return Controller.extend("com.pabz.ConfermaVincita.controller.Main", {
 
 		onConfirm: function () {
 			this.getOwnerComponent().completeTask(true);
-		};
+		},
 
 		onReject: function () {
 			this.getOwnerComponent().completeTask(false);
-		};
+		}
 
 	});
 });
